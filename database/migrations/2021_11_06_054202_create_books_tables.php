486af8fcd926afad9b253312de09ef9b48146d89
movingfastball <movingfastballny2012@yahoo.co.jp>
2021-11-06 17:37:55 +0900

    マイグレーション関連

---
+++ database/migrations/2021_11_06_054202_create_books_tables.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('item_name');
+            $table->integer('item_number');
+            $table->datetime('published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
