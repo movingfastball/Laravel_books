2b13c2e7f095d198742821981865cad6ed06a951
movingfastball <movingfastballny2012@yahoo.co.jp>
2021-11-10 17:19:23 +0900

    登録機能、削除機能追加＋マイグレーション修正

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -9,19 +9,31 @@
         <!--バリデーションエラーの表示で使用-->
         @include('common.errors')
 
-        <!--本登録フォーム-->
+        <!--本のタイトル-->
         <form action="{{ url('books') }}" method="POST" class="form-horizontal">
             @csrf
 
-            <!--本のタイトル-->
-            <div class="form-group">
-                <div class="col-sm-6">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="book" class="col-sm-3 control-label">Book</label>
                     <input type="text" name="item_name" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="book" class="col-sm-3 control-label">金額</label>
+                    <input type="text" name="item_amount" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="book" class="col-sm-3 control-label">数</label>
+                    <input type="text" name="item_number" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="book" class="col-sm-3 control-label">公開日</label>
+                    <input type="date" name="item_name" class="form-control">
                 </div>
             </div>
 
             <!--本登録ボタン-->
-            <div class="form-group">
+            <div class="form-row">
                 <div class="col-sm-offset-3 col-sm-6">
                     <button type="submit" class="btn btn-primary">
                         Save
@@ -33,5 +45,48 @@
     </div>
 
     <!--すでに登録されている本のリスト-->
+    @if(count($books) > 0)
+        <div class="card-body">
+            <div class="card-body">
+                <table class="table table-striped task-table">
+                    <thead>
+                        <th>本一覧</th>
+                        <th>&nbsp;</th>
+                    </thead>
+                    <tbody>
+                        @foreach($books as $book)
+                        <tr>
+                            <td class="table-text">
+                                <div>{{ $book->item_name }}</div>
+                            </td>
 
+                            <!--本:更新ボタン-->
+                            <td>
+                                <form action="{{ url('booksedit/'.$book->id) }}" method="POST">
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary">
+                                        更新
+                                    </button>
+                                </form>
+                            </td>
+
+                            </td>
+                            <!--本削除ボタン-->
+                            <td>
+                                <form action="{{ url('book/'.$book->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button type="submit" class="btn btn-danger">
+                                        削除
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
